76018ee511cf64a4e35bc0307563597b6f28e79a
NEUS GARCIA <neuspiano@gmail.com>
2022-08-02 12:54:45 +0200

    main functionality done

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 module.exports.filterData = (results) => {
     var artists = [];
     for (i = 0; i < results.length; i++) {
@@ -11,10 +13,31 @@ module.exports.filterData = (results) => {
     return artists;
 };
 
-module.exports.createCSV = (data, filename) => {
+module.exports.createCSV = (data) => {
     var csv = "name,mbid,url\r\n";
     for (let i of data) {
         csv += i.join(",") + "\r\n";
     }
     return csv;
 };
+
+/* module.exports.deleteFile = (file) => {
+    setTimeout(() => {
+        fs.unlink(file, (err) => {
+            if (err) {
+                console.log("err", err);
+            }
+            console.log("file deleted");
+        });
+    }, 5000);
+};
+
+module.exports.writeFile = (filename, csv) => {
+    fs.writeFile(`${filename}.csv`, csv, (err) => {
+        if (err) {
+            return console.log("error", err);
+        }
+        console.log("file created");
+    });
+};
+ */
--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 const express = require("express");
-const fs = require("fs");
 const fetch = require("node-fetch");
 
 const secrets = require("./secrets.json"); // the API keys
 const functions = require("./functions.js");
+const random_artists = require("./random_artists.json");
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.send(
+        '<html><p>To download the CSV file, add the following query to the URL: /"artist"/"filename"</p><p>Example: .../madonna/madonna_list</p></html>'
+    );
 });
 
-//this is the route that the client side can call in order to obtain the CSV file with the results.
-//It contains two params: :artist => artist name || :filename => the user-supplied file name.
+//this is the REST Api that the client side can call in order to obtain the CSV file with the results.
+//The route contains two params: :artist => artist name || :filename => the user-supplied file name.
 
-app.get("/api/:artist/:filename", (req, res) => {
+app.get("/:artist/:filename", (req, res) => {
     console.log("GET request to /api/filename");
 
     const { artist, filename } = req.params;
 
-    //fetching the Last.fm API using the :artist parameter and the imported API_KEY.
+    //Now we fetch the Last.fm API using the :artist parameter and the imported API_KEY. I decided to limit the number of results to 20.
 
     fetch(
-        `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artist}&api_key=${secrets.API_KEY}&format=json&limit=10`
+        `http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artist}&api_key=${secrets.API_KEY}&format=json&limit=20`
     )
         .then((res) => res.json())
         .then((data) => {
-            if (data) {
-                let results = data.results.artistmatches.artist;
-                const artists = functions.filterData(results); // Filtering the retrieved data
-
-                const csv = functions.createCSV(artists, filename);
-
-                fs.writeFile(`${filename}.csv`, csv, function (err) {
-                    if (err) {
-                        return console.log("error", err);
-                    }
-                    res.attachment(`${filename}.csv`);
-                    res.end();
-                });
+             if (data) {
+                //If there are results, we write them in a CSV file and send everything to the user:
+                let results = data.results.artistmatches.artist; //accessing the object values
+                const artists = functions.filterData(results); // filtering the retrieved data
+                const csv = functions.createCSV(artists); //building a string in CSV format
+                res.attachment(`${filename}.csv`).send(csv); // sending the CSV file to the user (download)
             }
+            //Otherwise, if there's no data we give the user some results from our JSON file.
             console.log("No results", data);
+            const csv = functions.createCSV(random_artists);
+            res.attachment(`${filename}.csv`).send(csv);
         })
         .catch((err) => {
             console.log("error getting an artist", err);
@@ -49,4 +47,4 @@ app.get("/api/:artist/:filename", (req, res) => {
         });
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port 3000!`));
+app.listen(port, () => console.log(`listening on port 3000!`));
